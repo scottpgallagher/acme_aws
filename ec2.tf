1f8421f6f397486b2df530fc14bd2c1c4fb4453e
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2024-11-19 15:16:33 -0500

    Update ec2.tf
    Removed extra "s"

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2_server" {
   instance_type = var.instance_type
   key_name               = var.aws_key_pair_name
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_id
+  vpc_security_group_id = var.security_group_id
   ebs_optimized          = true
 
   root_block_device {
