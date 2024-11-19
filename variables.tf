c00b30949a677354b32473375d5ca160936043f1
Scott Gallagher <scottpgallagher@icloud.com>
2024-11-19 14:51:24 -0500

    initial variables.tf file

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+}
+
+variable "aws_profile" {
+}
+
+variable "aws_key_pair_name" {
+}
+
+variable "ec2_ami" {
+}
+
+variable "instance_type" {
+}
+
+variable "ebs_size" {
+}
+
+variable "security_group_id" {
+}
+
+variable "vpc_id" {
+}
+
+variable "subnet_id" {
+}
