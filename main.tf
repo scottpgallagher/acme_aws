c88345e72563147194a19f6fdf36e810ac95d8f5
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2024-11-19 14:40:51 -0500

    Update main.tf
    Update to providers to include region var

--- main.tf
+++ main.tf
@@ -2,7 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 3.27"
     }
   }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region  = var.aws_region
 }
