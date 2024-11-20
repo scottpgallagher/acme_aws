ddd5e1bea7f576c4a0e1ea6fb209f06cb54e0f0c
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2024-11-20 09:58:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.39"
+      version = "~> 3.27"
     }
   }
+
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
