a1e95d15657fea6818b10cbe4a0b12b727b3a616
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2024-11-20 10:00:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 5.39"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
- profile = var.aws_profile
- region  = var.aws_region
- default_tags {
-   tags = {
-     "teleport.dev/creator" = "scott@goteleport.com"
-   }
- }
+  profile = var.aws_profile
+  region = var.aws_region
+  default_tags {
+    tags = {
+      "teleport.dev/creator" = scott@goteleport.com
+    }
+  }
 }
