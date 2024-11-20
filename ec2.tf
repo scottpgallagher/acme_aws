c55ce1e5b27a30855ac6a8e4a8393eead3dbe102
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2024-11-20 14:58:22 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_server" {
   
-  ami           = ami-0ea3c35c5c3284d82
-  instance_type = var.instance_type
+  ami                    = var.ec2_ami
+  instance_type          = var.instance_type
   key_name               = var.aws_key_pair_name
   subnet_id              = var.subnet_id
   ebs_optimized          = true
