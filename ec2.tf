940356b3de0ebddba84dd884d31c20e08b683d2e
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2024-11-20 15:11:24 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2_server" {
   
-  ami                    = "ami-0ea3c35c5c3284d8"
+  ami                    = "ami-0ea3c35c5c3284d82"
   instance_type          = var.instance_type
   key_name               = var.aws_key_pair_name
   subnet_id              = var.subnet_id
