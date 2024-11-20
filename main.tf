995977e9d908825419869541bd6059499140e9a0
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2024-11-20 10:02:56 -0500

    Update main.tf
    Added variable for tag

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = var.aws_region
   default_tags {
     tags = {
-      "teleport.dev/creator" = scott@goteleport.com
+      "teleport.dev/creator" = var.user
     }
   }
 }
