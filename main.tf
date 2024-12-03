032971c0eae516e784804798a87f5894a04612cc
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2024-12-03 11:17:52 -0500

    Update main.tf
    Added random_id

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+resource "random_id" "instance_id" {
+  byte_length = 4
+}
+
 provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
